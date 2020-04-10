5b6cd4f6b03af4baeba113e439cc9894f80a8ccc
Roland Puntaier <roland.puntaier@gmail.com>
2020-04-10 18:53:34 +0200

    v1.0.4

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def main():
     ]
     setuptools.setup(
         name="chcko",
-        version = "1.0.3",
+        version = "1.0.4",
         description="chcko randomly parameterized exercises automatically checked (formerly mamchecker)",
         long_description=readme,
         long_description_content_type="text/x-rst",
